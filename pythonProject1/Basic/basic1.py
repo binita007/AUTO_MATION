07994f7548dbd403c48143862ef6c9b8d50388b6
= <binita.tamang1991@gmail.com>
2024-08-30 14:42:52 +0545

    initila project added

---
+++ pythonProject1/Basic/basic1.py
@@ -0,0 +1,41 @@
+print('Hello World')
+
+
+#VARIABLES
+x = 5
+y = "Binita"
+print(x)
+print(y)
+
+
+x = 5
+y = 10
+print(x + y)
+
+
+x = "easier langage"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#if else
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#for loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
